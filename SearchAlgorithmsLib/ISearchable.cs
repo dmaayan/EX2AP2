149cb1bc4f78fbb5522ca829ed4f0b9aba823617
nweichse <nevo12345@gmail.com>
2017-04-01 20:40:57 +0300

    abstract ISearchable

---
+++ SearchAlgorithmsLib/ISearchable.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithmsLib
+{
+    public class State<T>
+    {
+        private T state; // the state represented by a string
+        private double cost; // cost to reach this state (set by a setter)
+        private State<T> cameFrom; // the state we came from to this state (setter)
+        public State(T state) // CTOR
+        {
+            this.state = state;
+        }
+        public bool Equals(State<T> s) // we overload Object's Equals method
+        {
+            return state.Equals(s.state);
+        }
+        // ...
+    }
+
+
+    public interface ISearchable<T>
+    {
+        State<T> getInitialState();
+        State<T> getGoalState();
+        List<State<T>> getAllPossibleStates(State<T> s);
+
+    }
+
+    public class Solution
+    {
+
+    }
+
+    public interface ISearcher<T>
+    {
+        // the search method
+        Solution search(ISearchable<T> searchable);
+        // get how many nodes were evaluated by the algorithm
+        int getNumberOfNodesEvaluated();
+    }
+
+    public abstract class Searcher<T> : ISearcher<T>
+    {
+        
+    }
+}
